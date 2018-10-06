69d93c6ad520095e89f006414d7764f162027c2d
lauraVzarco <laurav.zarco@gmail.com>
2018-10-06 15:22:26 +0200

    finished styles

--- src/Components/Element.js
+++ src/Components/Element.js
@@ -5,15 +5,28 @@ class Element extends Component {
 
     render() {
         return (
-            <div>
-                <div>
-                    <img src={this.props.image} alt="pokemon sprite" />
-                    <div>{this.props.id}</div>
+            <div className='pokemonCard'>
+                <div className='upCard'>
+                    <img
+                        src={this.props.image}
+                        className='pokeImage'
+                        alt="pokemon sprite" />
+                    <div className='pokeId'>
+                        ID / {this.props.id}
+                    </div>
                 </div>
-                <div>
-                    <div>{this.props.name}</div>
-                    <div>{this.props.type.map((types, index) =>
-                        <span key={index}> {types} </span>)}</div>
+                <div className='downCard'>
+                    <div className='pokeName'>
+                        {this.props.name}
+                    </div>
+                    <ul className='pokeType'>
+                        {this.props.type.map((types, index) =>
+                            <li
+                                key={index}
+                                className='type'>
+                                {types}
+                            </li>)}
+                    </ul>
                 </div>
             </div>
         );
