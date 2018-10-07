b9f07df32f1eec35fb0947f3d735e0ae9a32abb5
lauraVzarco <laurav.zarco@gmail.com>
2018-10-07 19:59:24 +0200

    to github pages

---
+++ docs/precache-manifest.41a46dccd5c2076aae04f392b20656d9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "56fbe5f9f35abc3ee58f",
+    "url": "./static/js/main.56fbe5f9.chunk.js"
+  },
+  {
+    "revision": "8bfb27a1cee6952d85ab",
+    "url": "./static/js/1.8bfb27a1.chunk.js"
+  },
+  {
+    "revision": "56fbe5f9f35abc3ee58f",
+    "url": "./static/css/main.a929cb26.chunk.css"
+  },
+  {
+    "revision": "da899e2b060e9783cf1e1cef482aff3f",
+    "url": "./index.html"
+  }
+];
